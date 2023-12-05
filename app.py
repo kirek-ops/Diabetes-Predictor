34726ee0a82ec6c54cad2df46593ae4d4eef3b1a
kirek <kirylmeleniako@gmail.com>
2023-12-05 21:38:28 +0100

    Something doesn't work, idk... I add missed fieild, but model give me errors, in incognito works

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import tensorflow as tf
+import numpy as np
 
 model = tf.keras.models.load_model('model.model')
 
@@ -7,15 +8,21 @@ app = Flask(__name__, template_folder = 'templates')
 
 @app.route('/', methods = ['GET', 'POST'])
 def index ():
+    prediction = None
     if request.method == 'POST':
         Pregnancies = request.form['Pregnancies']
+        BloodPressure = request.form['BloodPressure']
         Glucose = request.form['Glucose']
         SkinThickness = request.form['SkinThickness']
         Insulin = request.form['Insulin']
         BMI = request.form['BMI']
         DiabetesPedigreeFunction = request.form['DiabetesPedigreeFunction']
         Age = request.form['Age']
-        print(Pregnancies, Glucose, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age)
+        test = [int(Pregnancies), int(BloodPressure), int(Glucose), int(SkinThickness), int(Insulin), int(BMI), int(DiabetesPedigreeFunction), int(Age)]
+        test = np.array(test)
+        print(test.shape)
+        # prediction = model.predict(test)
+        # print(prediction)
 
     return render_template('index.html')
 
