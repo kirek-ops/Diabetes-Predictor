aa3a1b48b55024939414734b7e88bb27c02ac76d
kirek <kirylmeleniako@gmail.com>
2023-12-05 20:55:50 +0100

    Add more functional fields on form, create and build a model, add saving data after refreshing page

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import tensorflow as tf
+
+model = tf.keras.models.load_model('model.model')
 
 app = Flask(__name__, template_folder = 'templates')
 
-@app.route('/')
-def home ():
+@app.route('/', methods = ['GET', 'POST'])
+def index ():
+    if request.method == 'POST':
+        Pregnancies = request.form['Pregnancies']
+        Glucose = request.form['Glucose']
+        SkinThickness = request.form['SkinThickness']
+        Insulin = request.form['Insulin']
+        BMI = request.form['BMI']
+        DiabetesPedigreeFunction = request.form['DiabetesPedigreeFunction']
+        Age = request.form['Age']
+        print(Pregnancies, Glucose, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age)
+
     return render_template('index.html')
 
 if __name__ == "__main__":
---
+++ model.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from sklearn.preprocessing import MinMaxScaler
+
+# Import Data
+data_path = 'data/diabetes.csv'
+df = pd.read_csv(data_path)
+
+y = df['Outcome']
+X = df.drop('Outcome', axis = 1)
+
+scaler = MinMaxScaler()
+X = scaler.fit_transform(X)
+
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 137)
+
+model = Sequential([
+    Dense(7, activation = 'relu'),
+    Dense(7, activation = 'relu'),
+    Dense(6, activation = 'relu'),
+    Dense(4, activation = 'relu'),
+    Dense(2, activation = 'softmax'),
+])
+
+model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+model.fit(x_train, y_train, epochs = 5)
+
+predictions = model.predict(x_test)
+result = np.array([0 if x > y else 1 for x, y in predictions])
+
+model.save('model.model')
+
+loss = np.mean((result - y_test) ** 2)
+print(f"Loss: {loss}")
